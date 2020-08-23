321f1abfde32c57a6eb1c4564d75e1ef367329ef
casjay <git-admin@casjaysdev.com>
2020-08-23 19:52:01 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,9 @@
 #
 #change to match your setup
 STATICSITE="${STATICSITE:-static.casjay.net}"
-git clone https://github.com/casjay-templates/default-web-assets /usr/share/httpd
+
+[ -d /usr/share/httpd/.git ]&& echo "Updating Web Assets" && git -C /usr/share/httpd pull -q ||
+rm -Rf /usr/share/httpd && echo "Cloning Default Web Assets"
+git clone -q https://github.com/casjay-templates/default-web-assets /usr/share/httpd
+
 find /usr/share/httpd -not -path "./git/*" -type f -iname "*.php" -iname ".*html" -iname "*.md" -iname "*.css" -exec sed -i "s#static.casjay.net#$STATICSITE#g" {} \; >/dev/null 2>&1
