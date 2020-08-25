7dd823648a4f7c7de2fe9a3d4003fcbe63490bfe
casjay <git-admin@casjaysdev.com>
2020-08-25 17:59:54 -0400

    Update index.default.php

--- html/index.default.php
+++ html/index.default.php
@@ -14,12 +14,13 @@
 <br><br>
 
 <div class="c1"><h2>Welcome to your new site</h2> </div> <br>
-<center>The site you have visited has  <br>
+<center><h4>
+The site you have visited has  <br>
 just been setup and the user  <br>
 hasn't created a site yet. <br><br>
 Please come back soon as I'm sure the <br>
 site owner is working on it!
-</center>
+</h4></center>
 <br><br><br><br>
 
 
