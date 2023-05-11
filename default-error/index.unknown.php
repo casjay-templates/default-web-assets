9680f98503a8b79f164c56d699ebbece2311517a
casjay <git-admin@casjaysdev.com>
2023-05-11 02:42:26 -0400

    🗃️ Committing everything that changed 🗃️
    default-error/index.unknown.php

--- default-error/index.unknown.php
+++ default-error/index.unknown.php
@@ -19,7 +19,11 @@
     <link rel="stylesheet" href="/default-css/github/ribbon.css" />
     <link rel="stylesheet" href="/default-css/other/forms-min.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?Roboto&family=Roboto+Mono&display=swap" />
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css"
+    />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 
     <script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer></script>
@@ -34,26 +38,31 @@
     <title>Domain Doesn't Exist</title>
   </head>
   <body>
-    <br /><br />
-    <div class="c1">
-      <h2>
-        UMMMMM<br />
-        This site doesn't seem to exist<br />
-      </h2>
+    <div class="container vh-100">
       <br /><br />
-      <img alt="error" height="400" width="400" src="/default-error/images/oops.gif" />
-      <br /><br /><br />
-    </div>
-    <div class="c5">
-      <br />
-      <?php echo "Server Name: " . $_SERVER['SERVER_NAME'] . "<BR>"; echo "IP Address: " . $_SERVER['SERVER_ADDR'] . "<BR />"; ?> <br /><br />
-      Linux OsVer:
-      <?php echo shell_exec('cat /etc/redhat-release'); ?><br />
-      ConfigVer:
-      <?php echo shell_exec('cat /etc/casjaysdev/updates/versions/configs.txt'); ?>
-      <br /><br />
-      Powered by a Redhat based system<br />
-      <a href="https://redhat.com"> <img border="0" alt="Redhat/CentOS/Fedora/SL Linux" src="/default-icons/powered_by_redhat.jpg" /> </a><br />
+      <div class="c1">
+        <h2>
+          UMMMMM<br />
+          This site doesn't seem to exist<br />
+        </h2>
+        <br /><br />
+        <img alt="error" height="400" width="400" src="/default-error/images/oops.gif" />
+        <br /><br /><br />
+      </div>
+      <div class="c5">
+        <br />
+        <?php echo "Server Name: " . $_SERVER['SERVER_NAME'] . "<BR>"; echo "IP Address: " . $_SERVER['SERVER_ADDR'] .
+        "<BR />"; ?> <br /><br />
+        Linux OsVer:
+        <?php echo shell_exec('cat /etc/redhat-release'); ?><br />
+        ConfigVer:
+        <?php echo shell_exec('cat /etc/casjaysdev/updates/versions/configs.txt'); ?>
+        <br /><br />
+        Powered by a Redhat based system<br />
+        <a href="https://redhat.com">
+          <img border="0" alt="Redhat/CentOS/Fedora/SL Linux" src="/default-icons/powered_by_redhat.jpg" /> </a
+        ><br />
+      </div>
       <br /><br /><br /><br /><br />
     </div>
     <!-- Begin Casjays Developments Footer -->
