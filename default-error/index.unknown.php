f855d0aa9563b90100dd9ed91895a4b9568717a1
casjay <git-admin@casjaysdev.com>
2023-05-11 02:40:31 -0400

    🗃️ Committing everything that changed 🗃️
    default-error/index.unknown.php
    .prettierrc.json

--- default-error/index.unknown.php
+++ default-error/index.unknown.php
@@ -31,36 +31,35 @@
     <script src="/default-js/utils/bootstrap.bundle.min.js" async defer></script>
     <script src="https://cdn.passprotect.io/passprotect.min.js" defer></script>
     <!-- End Default Header-->
-<title>Domain Doesn't Exist</title>
-
-</head>
-<body>
-<br><br>
-<div class="c1">
-<h2>UMMMMM<br>
-This site doesn't seem to exist<br>
-<br><br><br><br><br>
-</div>
-<div class="c5">
-<br>
-<?php
-echo "Server Name: " . $_SERVER['SERVER_NAME'] . "<BR>";
-echo "IP Address: " . $_SERVER['SERVER_ADDR'] . "<BR>";
-?>
-<br><br>
-Linux OsVer: <?php echo shell_exec('cat /etc/redhat-release'); ?><br>
-ConfigVer: <?php echo shell_exec(
-  'cat /etc/casjaysdev/updates/versions/configs.txt'
-); ?>
-<br><br>
-Powered by a Redhat based system<br>
-<a href="https://redhat.com"> <img border="0" alt="Redhat/CentOS/Fedora/SL Linux" src="/default-icons/powered_by_redhat.jpg"> </a><br>
-<br><br><br><br><br>
-</div>
-<!-- Begin Casjays Developments Footer -->
-<center>
-<?php include "https://REPLACE_STATIC_HOSTNAME/default-html/casjays-footer.php"; ?>
-</center>
-<!-- End Casjays Developments Footer -->
-</body>
+    <title>Domain Doesn't Exist</title>
+  </head>
+  <body>
+    <br /><br />
+    <div class="c1">
+      <h2>
+        UMMMMM<br />
+        This site doesn't seem to exist<br />
+      </h2>
+      <br /><br />
+      <img alt="error" height="400" width="400" src="/default-error/images/oops.gif" />
+      <br /><br /><br />
+    </div>
+    <div class="c5">
+      <br />
+      <?php echo "Server Name: " . $_SERVER['SERVER_NAME'] . "<BR>"; echo "IP Address: " . $_SERVER['SERVER_ADDR'] . "<BR />"; ?> <br /><br />
+      Linux OsVer:
+      <?php echo shell_exec('cat /etc/redhat-release'); ?><br />
+      ConfigVer:
+      <?php echo shell_exec('cat /etc/casjaysdev/updates/versions/configs.txt'); ?>
+      <br /><br />
+      Powered by a Redhat based system<br />
+      <a href="https://redhat.com"> <img border="0" alt="Redhat/CentOS/Fedora/SL Linux" src="/default-icons/powered_by_redhat.jpg" /> </a><br />
+      <br /><br /><br /><br /><br />
+    </div>
+    <!-- Begin Casjays Developments Footer -->
+    <center>
+      <?php include "https://REPLACE_STATIC_HOSTNAME/default-html/casjays-footer.php"; ?>
+    </center>
+    <!-- End Casjays Developments Footer -->
+  </body>
 </html>
